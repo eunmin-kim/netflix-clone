93f6a8d50cd0b9696806e23dcbe4adf9c1f38a1d
eunmin-kim <53runtime@gmail.com>
2024-02-26 16:51:50 +0900

    Feature: 메인 화면 추가

---
+++ src/page/main/MainPage.jsx
@@ -0,0 +1,14 @@
+import {View,Text} from "react-native";
+
+
+function MainPage() {
+    return (
+        <View>
+            <Text>
+                Main Page!
+            </Text>
+        </View>
+    )
+}
+
+export default MainPage;
